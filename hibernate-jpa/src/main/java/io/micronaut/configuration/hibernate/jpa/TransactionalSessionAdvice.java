9cff587ecdee46a6f34150aa012d1abcf1238636
graemerocher <graeme.rocher@gmail.com>
2020-03-18 10:52:43 +0100

    Checkstyle

--- hibernate-jpa/src/main/java/io/micronaut/configuration/hibernate/jpa/TransactionalSessionAdvice.java
+++ hibernate-jpa/src/main/java/io/micronaut/configuration/hibernate/jpa/TransactionalSessionAdvice.java
@@ -3,9 +3,6 @@ package io.micronaut.configuration.hibernate.jpa;
 import io.micronaut.aop.Introduction;
 import io.micronaut.context.annotation.Type;
 import io.micronaut.core.annotation.Internal;
-import io.micronaut.transaction.jdbc.TransactionalConnection;
-import io.micronaut.transaction.jdbc.TransactionalConnectionInterceptor;
-
 import java.lang.annotation.Retention;
 
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
@@ -14,8 +11,8 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
  * An introduction advice annotation used to create a transaction aware session.
  * Considered internal and not for explicit usage.
  *
- * @see TransactionalConnectionInterceptor
- * @see TransactionalConnection
+ * @see TransactionalSessionInterceptor
+ * @see TransactionalSession
  */
 @Retention(RUNTIME)
 @Introduction
