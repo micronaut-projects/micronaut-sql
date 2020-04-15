13fa5f3743e5eab7920354278cd14024fcbc9177
recursivecodes <toddraymondsharp@gmail.com>
2020-04-15 10:19:54 -0400

    switch config to extend PoolDataSourceImpl

--- jdbc-ucp/src/main/java/io/micronaut/configuration/jdbc/ucp/DatasourceConfiguration.java
+++ jdbc-ucp/src/main/java/io/micronaut/configuration/jdbc/ucp/DatasourceConfiguration.java
@@ -20,6 +20,7 @@ import io.micronaut.context.annotation.Parameter;
 import io.micronaut.jdbc.BasicJdbcConfiguration;
 import io.micronaut.jdbc.CalculatedSettings;
 import oracle.ucp.jdbc.PoolDataSource;
+import oracle.ucp.jdbc.PoolDataSourceImpl;
 
 /**
  * Allows the configuration of UCP JDBC data sources. All properties on
@@ -34,44 +35,10 @@ import oracle.ucp.jdbc.PoolDataSource;
  * @since 2.0.1
  */
 @EachProperty(value = BasicJdbcConfiguration.PREFIX, primary = "default")
-public class DatasourceConfiguration implements BasicJdbcConfiguration {
+public class DatasourceConfiguration extends PoolDataSourceImpl implements BasicJdbcConfiguration {
 
     public CalculatedSettings calculatedSettings;
     private String name;
-    private String connectionFactoryClassName = "oracle.jdbc.pool.OracleDataSource";
-    private String driverClassName;
-    private String connectionPoolName;
-    private String url;
-    private String username;
-    private String password;
-    private Integer initialPoolSize = 5;
-    private Integer minPoolSize = 5;
-    private Integer maxPoolSize = 20;
-    private Integer timeoutCheckInterval = 5;
-    private Integer inactiveConnectionTimeout = 10;
-
-    private Integer connectionWaitTimeout;
-    private Integer loginTimeout;
-    private Integer maxConnectionReuseCount;
-    private Integer maxConnectionReuseTime;
-    private Integer maxIdleTime;
-    private Integer maxStatements;
-    private String networkProtocol;
-    private String onsConfiguration;
-    private Integer portNumber;
-    private Integer propertyCycle;
-    private String roleName;
-    private String serverName;
-    private String sqlForValidateConnection;
-    private Boolean fastConnectionFailoverEnabled;
-    private Boolean validateConnectionOnBorrow;
-
-    private Boolean fixedString = false;
-    private Boolean remarksReporting = false;
-    private Boolean restrictGetTables = false;
-    private Boolean includeSynonyms = false;
-    private Boolean defaultNChar = false;
-    private Boolean accumulateBatchResult = false;
 
     /**
      * Constructor.
@@ -80,246 +47,6 @@ public class DatasourceConfiguration implements BasicJdbcConfiguration {
     public DatasourceConfiguration(@Parameter String name) {
         this.name = name;
         this.calculatedSettings = new CalculatedSettings(this);
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getMaxConnectionReuseTime() {
-        return maxConnectionReuseTime;
-    }
-
-    public void setMaxConnectionReuseTime(Integer maxConnectionReuseTime) {
-        this.maxConnectionReuseTime = maxConnectionReuseTime;
-    }
-
-    public Integer getConnectionWaitTimeout() {
-        return connectionWaitTimeout;
-    }
-
-    public void setConnectionWaitTimeout(Integer connectionWaitTimeout) {
-        this.connectionWaitTimeout = connectionWaitTimeout;
-    }
-
-    public Integer getLoginTimeout() {
-        return loginTimeout;
-    }
-
-    public void setLoginTimeout(Integer loginTimeout) {
-        this.loginTimeout = loginTimeout;
-    }
-
-    public Integer getMaxConnectionReuseCount() {
-        return maxConnectionReuseCount;
-    }
-
-    public void setMaxConnectionReuseCount(Integer maxConnectionReuseCount) {
-        this.maxConnectionReuseCount = maxConnectionReuseCount;
-    }
-
-    public Integer getMaxIdleTime() {
-        return maxIdleTime;
-    }
-
-    public void setMaxIdleTime(Integer maxIdleTime) {
-        this.maxIdleTime = maxIdleTime;
-    }
-
-    public Integer getMaxStatements() {
-        return maxStatements;
-    }
-
-    public void setMaxStatements(Integer maxStatements) {
-        this.maxStatements = maxStatements;
-    }
-
-    public String getNetworkProtocol() {
-        return networkProtocol;
-    }
-
-    public void setNetworkProtocol(String networkProtocol) {
-        this.networkProtocol = networkProtocol;
-    }
-
-    public String getOnsConfiguration() {
-        return onsConfiguration;
-    }
-
-    public void setOnsConfiguration(String onsConfiguration) {
-        this.onsConfiguration = onsConfiguration;
-    }
-
-    public Integer getPortNumber() {
-        return portNumber;
-    }
-
-    public void setPortNumber(Integer portNumber) {
-        this.portNumber = portNumber;
-    }
-
-    public Integer getPropertyCycle() {
-        return propertyCycle;
-    }
-
-    public void setPropertyCycle(Integer propertyCycle) {
-        this.propertyCycle = propertyCycle;
-    }
-
-    public String getRoleName() {
-        return roleName;
-    }
-
-    public void setRoleName(String roleName) {
-        this.roleName = roleName;
-    }
-
-    public String getServerName() {
-        return serverName;
-    }
-
-    public void setServerName(String serverName) {
-        this.serverName = serverName;
-    }
-
-    public String getSqlForValidateConnection() {
-        return sqlForValidateConnection;
-    }
-
-    public void setSqlForValidateConnection(String sqlForValidateConnection) {
-        this.sqlForValidateConnection = sqlForValidateConnection;
-    }
-
-    public Boolean isFastConnectionFailoverEnabled() {
-        return fastConnectionFailoverEnabled;
-    }
-
-    public void setFastConnectionFailoverEnabled(Boolean fastConnectionFailoverEnabled) {
-        this.fastConnectionFailoverEnabled = fastConnectionFailoverEnabled;
-    }
-
-    public Boolean isValidateConnectionOnBorrow() {
-        return validateConnectionOnBorrow;
-    }
-
-    public void setValidateConnectionOnBorrow(Boolean validateConnectionOnBorrow) {
-        this.validateConnectionOnBorrow = validateConnectionOnBorrow;
-    }
-
-    public String getConnectionFactoryClassName() {
-        return connectionFactoryClassName;
-    }
-
-    public void setConnectionFactoryClassName(String connectionFactoryClassName) {
-        this.connectionFactoryClassName = connectionFactoryClassName;
-    }
-
-    public void setDriverClassName(String driverClassName) {
-        this.driverClassName = driverClassName;
-    }
-
-    public String getConnectionPoolName() {
-        return connectionPoolName;
-    }
-
-    public void setConnectionPoolName(String connectionPoolName) {
-        this.connectionPoolName = connectionPoolName;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Integer getMinPoolSize() {
-        return minPoolSize;
-    }
-
-    public void setMinPoolSize(Integer minPoolSize) {
-        this.minPoolSize = minPoolSize;
-    }
-
-    public Integer getMaxPoolSize() {
-        return maxPoolSize;
-    }
-
-    public void setMaxPoolSize(Integer maxPoolSize) {
-        this.maxPoolSize = maxPoolSize;
-    }
-
-    public Integer getTimeoutCheckInterval() {
-        return timeoutCheckInterval;
-    }
-
-    public void setTimeoutCheckInterval(Integer timeoutCheckInterval) {
-        this.timeoutCheckInterval = timeoutCheckInterval;
-    }
-
-    public Integer getInactiveConnectionTimeout() {
-        return inactiveConnectionTimeout;
-    }
-
-    public void setInactiveConnectionTimeout(Integer inactiveConnectionTimeout) {
-        this.inactiveConnectionTimeout = inactiveConnectionTimeout;
-    }
-
-    public Boolean isFixedString() {
-        return fixedString;
-    }
-
-    public void setFixedString(Boolean fixedString) {
-        this.fixedString = fixedString;
-    }
-
-    public Boolean isRemarksReporting() {
-        return remarksReporting;
-    }
-
-    public void setRemarksReporting(Boolean remarksReporting) {
-        this.remarksReporting = remarksReporting;
-    }
-
-    public Boolean isRestrictGetTables() {
-        return restrictGetTables;
-    }
-
-    public void setRestrictGetTables(Boolean restrictGetTables) {
-        this.restrictGetTables = restrictGetTables;
-    }
-
-    public Boolean isIncludeSynonyms() {
-        return includeSynonyms;
-    }
-
-    public void setIncludeSynonyms(Boolean includeSynonyms) {
-        this.includeSynonyms = includeSynonyms;
-    }
-
-    public Boolean isDefaultNChar() {
-        return defaultNChar;
-    }
-
-    public void setDefaultNChar(Boolean defaultNChar) {
-        this.defaultNChar = defaultNChar;
-    }
-
-    public Boolean isAccumulateBatchResult() {
-        return accumulateBatchResult;
-    }
-
-    public void setAccumulateBatchResult(Boolean accumulateBatchResult) {
-        this.accumulateBatchResult = accumulateBatchResult;
-    }
-
-    public Integer getInitialPoolSize() {
-        return initialPoolSize;
-    }
-
-    public void setInitialPoolSize(Integer initialPoolSize) {
-        this.initialPoolSize = initialPoolSize;
     }
 
     public CalculatedSettings getCalculatedSettings() {
@@ -334,6 +61,9 @@ public class DatasourceConfiguration implements BasicJdbcConfiguration {
     public String getName() {
         return name;
     }
+    public void setName(String name) {
+        this.name = name;
+    }
 
     @Override
     public String getDriverClassName() {
@@ -342,41 +72,37 @@ public class DatasourceConfiguration implements BasicJdbcConfiguration {
 
     @Override
     public String getConfiguredDriverClassName() {
-        return this.driverClassName;
-    }
-
-    @Override
-    public String getUrl() {
-        return this.url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
+        return super.getConnectionFactoryClassName();
     }
 
     @Override
     public String getConfiguredUrl() {
-        return this.url;
+        return super.getURL();
+    }
+
+    @Override
+    public String getUrl() {
+        return calculatedSettings.getUrl();
     }
 
     @Override
     public String getUsername() {
-        return this.username;
+        return calculatedSettings.getUsername();
     }
 
     @Override
     public String getConfiguredUsername() {
-        return this.username;
+        return super.getUser();
     }
 
     @Override
     public String getPassword() {
-        return this.password;
+        return calculatedSettings.getPassword();
     }
 
     @Override
     public String getConfiguredPassword() {
-        return this.password;
+        return super.getPassword();
     }
 
     @Override
@@ -386,6 +112,6 @@ public class DatasourceConfiguration implements BasicJdbcConfiguration {
 
     @Override
     public String getConfiguredValidationQuery() {
-        return this.getValidationQuery();
+        return super.getSQLForValidateConnection();
     }
 }
--- jdbc-ucp/src/main/java/io/micronaut/configuration/jdbc/ucp/DatasourceFactory.java
+++ jdbc-ucp/src/main/java/io/micronaut/configuration/jdbc/ucp/DatasourceFactory.java
@@ -20,7 +20,6 @@ import io.micronaut.context.annotation.EachBean;
 import io.micronaut.context.annotation.Factory;
 import oracle.ucp.admin.UniversalConnectionPoolManagerImpl;
 import oracle.ucp.jdbc.PoolDataSource;
-import oracle.ucp.jdbc.PoolDataSourceFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -28,7 +27,6 @@ import javax.annotation.PreDestroy;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Properties;
 
 /**
  * Creates a UCP data source for each configuration bean.
@@ -42,50 +40,14 @@ public class DatasourceFactory implements AutoCloseable {
     private List<PoolDataSource> dataSources = new ArrayList<>(2);
 
     /**
-     * @param datasourceConfiguration A {@link DatasourceConfiguration}
+     * @param datasource A {@link DatasourceConfiguration}
      * @return A UCP {@link PoolDataSource}
      */
     @Context
     @EachBean(DatasourceConfiguration.class)
-    public PoolDataSource dataSource(DatasourceConfiguration datasourceConfiguration) throws SQLException {
-        PoolDataSource ds = PoolDataSourceFactory.getPoolDataSource();
-        ds.setConnectionFactoryClassName(datasourceConfiguration.getConnectionFactoryClassName());
-        ds.setURL(datasourceConfiguration.getUrl());
-        ds.setUser(datasourceConfiguration.getUsername());
-        ds.setPassword(datasourceConfiguration.getPassword());
-        ds.setConnectionPoolName(datasourceConfiguration.getConnectionPoolName());
-        ds.setInitialPoolSize(datasourceConfiguration.getInitialPoolSize());
-        ds.setMinPoolSize(datasourceConfiguration.getMinPoolSize());
-        ds.setMaxPoolSize(datasourceConfiguration.getMaxPoolSize());
-        ds.setTimeoutCheckInterval(datasourceConfiguration.getTimeoutCheckInterval());
-        ds.setInactiveConnectionTimeout(datasourceConfiguration.getInactiveConnectionTimeout());
-        Properties props = new Properties();
-        props.setProperty("fixedString", String.valueOf(datasourceConfiguration.isFixedString()));
-        props.setProperty("remarksReporting", String.valueOf(datasourceConfiguration.isRemarksReporting()));
-        props.setProperty("restrictGetTables", String.valueOf(datasourceConfiguration.isRestrictGetTables()));
-        props.setProperty("includeSynonyms", String.valueOf(datasourceConfiguration.isIncludeSynonyms()));
-        props.setProperty("defaultNChar", String.valueOf(datasourceConfiguration.isDefaultNChar()));
-        props.setProperty("AccumulateBatchResult", String.valueOf(datasourceConfiguration.isAccumulateBatchResult()));
-        ds.setConnectionProperties(props);
-
-        if (datasourceConfiguration.getConnectionWaitTimeout() != null) ds.setConnectionWaitTimeout(datasourceConfiguration.getConnectionWaitTimeout());
-        if (datasourceConfiguration.getLoginTimeout() != null) ds.setLoginTimeout(datasourceConfiguration.getLoginTimeout());
-        if (datasourceConfiguration.getMaxConnectionReuseCount() != null) ds.setMaxConnectionReuseCount(datasourceConfiguration.getMaxConnectionReuseCount());
-        if (datasourceConfiguration.getMaxConnectionReuseTime() != null) ds.setMaxConnectionReuseTime(datasourceConfiguration.getMaxConnectionReuseTime());
-        if (datasourceConfiguration.getMaxIdleTime() != null) ds.setMaxIdleTime(datasourceConfiguration.getMaxIdleTime());
-        if (datasourceConfiguration.getMaxStatements() != null) ds.setMaxStatements(datasourceConfiguration.getMaxStatements());
-        if (datasourceConfiguration.getNetworkProtocol() != null) ds.setNetworkProtocol(datasourceConfiguration.getNetworkProtocol());
-        if (datasourceConfiguration.getOnsConfiguration() != null) ds.setONSConfiguration(datasourceConfiguration.getOnsConfiguration());
-        if (datasourceConfiguration.getPortNumber() != null) ds.setPortNumber(datasourceConfiguration.getPortNumber());
-        if (datasourceConfiguration.getPropertyCycle() != null) ds.setPropertyCycle(datasourceConfiguration.getPropertyCycle());
-        if (datasourceConfiguration.getRoleName() != null) ds.setRoleName(datasourceConfiguration.getRoleName());
-        if (datasourceConfiguration.getServerName() != null) ds.setServerName(datasourceConfiguration.getServerName());
-        if (datasourceConfiguration.getSqlForValidateConnection() != null) ds.setSQLForValidateConnection(datasourceConfiguration.getSqlForValidateConnection());
-        if (datasourceConfiguration.isFastConnectionFailoverEnabled() != null) ds.setFastConnectionFailoverEnabled(datasourceConfiguration.isFastConnectionFailoverEnabled());
-        if (datasourceConfiguration.isValidateConnectionOnBorrow() != null) ds.setValidateConnectionOnBorrow(datasourceConfiguration.isValidateConnectionOnBorrow());
-
-        dataSources.add(ds);
-        return ds;
+    public PoolDataSource dataSource(DatasourceConfiguration datasource) {
+        dataSources.add(datasource);
+        return datasource;
     }
 
     @Override
