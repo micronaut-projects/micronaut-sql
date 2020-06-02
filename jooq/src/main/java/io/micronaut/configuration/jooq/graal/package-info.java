af849e11e0a990946482bd6a32dc72ed71fc2392
graemerocher <graeme.rocher@gmail.com>
2020-06-02 16:54:51 +0200

    Spotless fixes

--- jooq/src/main/java/io/micronaut/configuration/jooq/graal/package-info.java
+++ jooq/src/main/java/io/micronaut/configuration/jooq/graal/package-info.java
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 /**
  * This package contains GraalVM Substrate specific classes.
  *
