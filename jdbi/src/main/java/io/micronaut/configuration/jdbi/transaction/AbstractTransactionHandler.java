253e6f5922c8e5b8d59640dd8d1cc377c6bb2054
Iván López <lopezi@objectcomputing.com>
2020-06-15 13:04:59 +0200

    Fix license headers

--- jdbi/src/main/java/io/micronaut/configuration/jdbi/transaction/AbstractTransactionHandler.java
+++ jdbi/src/main/java/io/micronaut/configuration/jdbi/transaction/AbstractTransactionHandler.java
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package io.micronaut.configuration.jdbi.transaction;
 
 import org.jdbi.v3.core.Handle;
