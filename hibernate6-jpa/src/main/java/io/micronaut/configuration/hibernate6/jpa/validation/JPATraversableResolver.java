dc049882e915610e899cc7282e99b0c3a28df9f3
radovanradic <radicr@gmail.com>
2023-01-09 16:47:44 +0100

    Undo attempt to skip jacoco report

--- hibernate6-jpa/src/main/java/io/micronaut/configuration/hibernate6/jpa/validation/JPATraversableResolver.java
+++ hibernate6-jpa/src/main/java/io/micronaut/configuration/hibernate6/jpa/validation/JPATraversableResolver.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.micronaut.configuration.hibernate.jpa.validation;
+package io.micronaut.configuration.hibernate6.jpa.validation;
 
 import jakarta.inject.Singleton;
 import jakarta.persistence.Persistence;
