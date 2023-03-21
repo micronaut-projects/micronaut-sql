ab739d9627dff040ba0b0b69ab3d15bc20ded499
radovanradic <radicr@gmail.com>
2023-03-21 09:32:42 +0100

    Removed unused import

--- hibernate-jpa/src/main/java/io/micronaut/configuration/hibernate/jpa/proxy/IntroducedHibernateProxyFactory.java
+++ hibernate-jpa/src/main/java/io/micronaut/configuration/hibernate/jpa/proxy/IntroducedHibernateProxyFactory.java
@@ -28,7 +28,6 @@ import org.hibernate.type.CompositeType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.Serializable;
 import java.lang.reflect.Method;
 import java.util.Collections;
 import java.util.Set;
