19a1bb3f7493b7a634757b4c01bda702d6793b24
Milenko Supic <supic82@gmail.com>
2023-04-05 10:21:13 +0200

    Added metadata repository and modified tests

--- tests/hibernate6/hibernate6-mariadb/src/test/java/example/hibernate6/sync/MariaDBApp.java
+++ tests/hibernate6/hibernate6-mariadb/src/test/java/example/hibernate6/sync/MariaDBApp.java
@@ -21,7 +21,7 @@ import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 
 @MicronautTest
 @Property(name = "datasources.default.db-type", value = "mariadb")
-@Property(name = "jpa.default.properties.hibernate.dialect", value = "org.hibernate.dialect.MariaDB103Dialect")
+@Property(name = "jpa.default.properties.hibernate.dialect", value = "org.hibernate.dialect.MariaDBDialect")
 @Property(name = "test-resources.containers.mariadb.image-name", value = "mariadb:10.9.3")
 public class MariaDBApp extends AbstractApp {
 }
--- tests/hibernate6/hibernate6-oracle/src/test/java/example/hibernate6/sync/OracleDBApp.java
+++ tests/hibernate6/hibernate6-oracle/src/test/java/example/hibernate6/sync/OracleDBApp.java
@@ -21,7 +21,7 @@ import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 
 @MicronautTest
 @Property(name = "datasources.default.db-type", value = "oracle")
-@Property(name = "jpa.default.properties.hibernate.dialect", value = "org.hibernate.dialect.Oracle12cDialect")
+@Property(name = "jpa.default.properties.hibernate.dialect", value = "org.hibernate.dialect.OracleDialect")
 @Property(name = "test-resources.containers.oracle.image-name", value = "gvenzl/oracle-xe:slim")
 public class OracleDBApp extends AbstractApp {
 }
