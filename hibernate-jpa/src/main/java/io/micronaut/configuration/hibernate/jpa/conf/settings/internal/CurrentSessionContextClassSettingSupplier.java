3348d6178e494434accc3d1ed11e5b38824273f4
Denis Stepanov <denis.s.stepanov@oracle.com>
2023-06-13 08:53:16 -0500

    Don't use deprecated class from Micronaut Data

--- hibernate-jpa/src/main/java/io/micronaut/configuration/hibernate/jpa/conf/settings/internal/CurrentSessionContextClassSettingSupplier.java
+++ hibernate-jpa/src/main/java/io/micronaut/configuration/hibernate/jpa/conf/settings/internal/CurrentSessionContextClassSettingSupplier.java
@@ -22,7 +22,7 @@ import io.micronaut.context.BeanProvider;
 import io.micronaut.context.annotation.Prototype;
 import io.micronaut.context.annotation.Requires;
 import io.micronaut.core.annotation.Internal;
-import io.micronaut.transaction.hibernate6.MicronautSessionContext;
+import io.micronaut.transaction.hibernate.MicronautSessionContext;
 import org.hibernate.cfg.AvailableSettings;
 
 import java.util.Collections;
