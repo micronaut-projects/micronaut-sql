05c5adb02b6e7dcecb94a432cf1662004d10acd8
Sergio del Amo <sergio.delamo@softamo.com>
2023-07-13 12:57:23 +0200

    Disable JPA via configuration (#881)

---
+++ tests/hibernate6/hibernate6-mysql-disabled/src/test/java/example/hibernate6/sync/TempTest.java
@@ -0,0 +1,23 @@
+package example.hibernate6.sync;
+
+import io.micronaut.context.annotation.Property;
+import io.micronaut.core.util.StringUtils;
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import jakarta.inject.Inject;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+@Property(name = "jpa.enabled", value = StringUtils.FALSE)
+@MicronautTest
+class TempTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
