d26cd04ba08636b06724615df3f729218a4d10f5
radovanradic <radicr@gmail.com>
2024-01-25 21:20:11 +0100

    Update mysql docker container to 8.2

--- tests/hibernate6/hibernate6-reactive-mysql/src/test/java/example/hibernate6/reactive/MySQLApp.java
+++ tests/hibernate6/hibernate6-reactive-mysql/src/test/java/example/hibernate6/reactive/MySQLApp.java
@@ -22,6 +22,6 @@ import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 @MicronautTest(transactional = false)
 @Property(name = "jpa.default.properties.hibernate.connection.db-type", value = "mysql")
 @Property(name = "jpa.default.reactive", value = "true")
-@Property(name = "test-resources.containers.mysql.image-name", value = "mysql:8.0.30")
+@Property(name = "test-resources.containers.mysql.image-name", value = "mysql:8.2")
 public class MySQLApp extends AbstractApp {
 }
