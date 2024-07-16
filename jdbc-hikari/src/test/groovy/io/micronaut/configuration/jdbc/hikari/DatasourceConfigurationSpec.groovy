cedfc88aff9e6118c46038042f46656623b4f189
radovanradic <radicr@gmail.com>
2024-07-16 14:36:08 +0200

    H2 version is upgraded to 2.3.230

--- jdbc-hikari/src/test/groovy/io/micronaut/configuration/jdbc/hikari/DatasourceConfigurationSpec.groovy
+++ jdbc-hikari/src/test/groovy/io/micronaut/configuration/jdbc/hikari/DatasourceConfigurationSpec.groovy
@@ -117,7 +117,7 @@ class DatasourceConfigurationSpec extends Specification {
         String version = resultSet.getString(1)
 
         then:
-        version == '2.2.224'
+        version == '2.3.230'
 
         cleanup:
         applicationContext.close()
