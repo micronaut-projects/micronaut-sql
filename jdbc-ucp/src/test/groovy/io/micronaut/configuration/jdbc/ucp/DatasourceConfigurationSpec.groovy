7e47216f8df5a513b46f15bfc6c8ebffdcea9c57
radovanradic <radicr@gmail.com>
2025-06-05 11:47:12 +0200

    Added configuration option to control Borrowing Connections from UCP

--- jdbc-ucp/src/test/groovy/io/micronaut/configuration/jdbc/ucp/DatasourceConfigurationSpec.groovy
+++ jdbc-ucp/src/test/groovy/io/micronaut/configuration/jdbc/ucp/DatasourceConfigurationSpec.groovy
@@ -392,7 +392,8 @@ class DatasourceConfigurationSpec extends Specification {
         ApplicationContext applicationContext = new DefaultApplicationContext("test")
         applicationContext.environment.addPropertySource(MapPropertySource.of(
                 'test',
-                ["datasources.default.data-source-properties": ["oracle.fan.enabled": true]]
+                ["datasources.default.data-source-properties": ["oracle.fan.enabled": true],
+                 "oracle.ucp.createConnectionInBorrowThread": "true"]
         ))
         applicationContext.start()
         DataSourceResolver dataSourceResolver =  applicationContext.findBean(DataSourceResolver).orElse(DataSourceResolver.DEFAULT)
