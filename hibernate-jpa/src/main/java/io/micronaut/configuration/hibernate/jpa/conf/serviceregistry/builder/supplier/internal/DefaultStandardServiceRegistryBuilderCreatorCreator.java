77484af86a7c01e4755cec56097854a12b367870
radovanradic <radicr@gmail.com>
2025-11-05 15:50:16 +0100

    Remove unused code

--- hibernate-jpa/src/main/java/io/micronaut/configuration/hibernate/jpa/conf/serviceregistry/builder/supplier/internal/DefaultStandardServiceRegistryBuilderCreatorCreator.java
+++ hibernate-jpa/src/main/java/io/micronaut/configuration/hibernate/jpa/conf/serviceregistry/builder/supplier/internal/DefaultStandardServiceRegistryBuilderCreatorCreator.java
@@ -27,8 +27,6 @@ import org.hibernate.boot.registry.BootstrapServiceRegistry;
 import org.hibernate.boot.registry.BootstrapServiceRegistryBuilder;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.integrator.spi.Integrator;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * Default supplier of {@link StandardServiceRegistryBuilderCreator}.
@@ -40,8 +38,6 @@ import org.slf4j.LoggerFactory;
 @Requires(missingBeans = StandardServiceRegistryBuilderCreator.class)
 @Prototype
 final class DefaultStandardServiceRegistryBuilderCreatorCreator implements StandardServiceRegistryBuilderCreator {
-
-    private static final Logger LOG = LoggerFactory.getLogger(DefaultStandardServiceRegistryBuilderCreatorCreator.class);
 
     private final BootstrapServiceRegistry bootstrapServiceRegistry;
 
@@ -56,14 +52,6 @@ final class DefaultStandardServiceRegistryBuilderCreatorCreator implements Stand
 
     @Override
     public StandardServiceRegistryBuilder create(JpaConfiguration jpaConfiguration) {
-        if (jpaConfiguration.isCompileTimeHibernateProxies()) {
-            // TODO: org.hibernate.cfg.AvailableSettings.BYTECODE_PROVIDER is deprecated and will be removed
-            // At present only bytebuddy is supported, bytebuddy being the default since version 5.3.
-            // Hibernate ORM will use the BytecodeProvider implementation it finds on the
-            // classpath loading it via the standard ServiceLoader mechanism. Currently, there is only a single
-            // implementation which is included in Hibernate ORM, so it's not possible to override this.
-            LOG.debug("Creating StandardServiceRegistry with compile-time Hibernate Proxy implementations.");
-        }
         if (jpaConfiguration.isReactive()) {
             throw new IllegalStateException("Hibernate Reactive not found on classpath!");
         }
