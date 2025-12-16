0065b88e737b83d34432c3103178d70b2196dd54
radovanradic <radicr@gmail.com>
2025-12-16 15:42:59 +0100

    Remove commented out code

--- jdbc-dbcp/src/test/groovy/io/micronaut/configuration/jdbc/dbcp/DatasourceConfigurationSpec.groovy
+++ jdbc-dbcp/src/test/groovy/io/micronaut/configuration/jdbc/dbcp/DatasourceConfigurationSpec.groovy
@@ -244,9 +244,6 @@ class DatasourceConfigurationSpec extends Specification {
 
         then:
         dataSource.maxWaitMillis == 5000
-        // TODO: There is no public getter, was it there in 4.x?
-        // dataSource.connectionProperties.getProperty('prop1') == 'value1'
-        // dataSource.connectionProperties.getProperty('prop2') == 'value2'
         dataSource.defaultAutoCommit
         dataSource.defaultCatalog == 'catalog'
 
