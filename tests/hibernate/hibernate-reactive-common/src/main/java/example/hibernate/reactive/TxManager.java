1d8ce4a127403aaf31e5417562b193ac3c8367af
radovanradic <radicr@gmail.com>
2026-03-03 12:24:42 +0100

    Update to micronaut-data 5.0

--- tests/hibernate/hibernate-reactive-common/src/main/java/example/hibernate/reactive/TxManager.java
+++ tests/hibernate/hibernate-reactive-common/src/main/java/example/hibernate/reactive/TxManager.java
@@ -1,23 +1,52 @@
 package example.hibernate.reactive;
 
 import io.micronaut.context.annotation.EachBean;
-import org.jspecify.annotations.NonNull;
+import io.micronaut.core.async.propagation.ReactorPropagation;
 import io.micronaut.data.connection.ConnectionDefinition;
 import io.micronaut.data.connection.ConnectionStatus;
 import io.micronaut.data.connection.DefaultConnectionDefinition;
-import io.micronaut.data.connection.support.DefaultConnectionStatus;
+import io.micronaut.data.connection.reactive.DefaultReactiveConnectionStatus;
+import io.micronaut.data.connection.reactive.ReactorConnectionOperations;
 import io.micronaut.transaction.TransactionDefinition;
 import io.micronaut.transaction.reactive.ReactiveTransactionOperations;
 import io.micronaut.transaction.reactive.ReactiveTransactionStatus;
+import org.jspecify.annotations.NonNull;
 import org.hibernate.SessionFactory;
 import org.hibernate.reactive.stage.Stage;
 import org.reactivestreams.Publisher;
 import reactor.core.publisher.Mono;
+import reactor.util.context.ContextView;
+
+import java.util.Optional;
 
 @EachBean(SessionFactory.class)
 public class TxManager implements ReactiveTransactionOperations<Stage.Session> {
 
     public static final String SESSION_KEY = "HibernateReactiveSession";
+
+    private final ReactorConnectionOperations<Stage.Session> connectionOperations = new ReactorConnectionOperations<>() {
+        @Override
+        public Optional<ConnectionStatus<Stage.Session>> findConnectionStatus(@NonNull ContextView contextView) {
+            return ReactorPropagation.findAllContextElements(contextView, ConnectionStatus.class)
+                .filter(e -> managesConnection(e))
+                .map(status -> (ConnectionStatus<Stage.Session>) status)
+                .findFirst();
+        }
+
+        @Override
+        public boolean managesConnection(@NonNull ConnectionStatus<Stage.Session> connectionStatus) {
+            if (!(connectionStatus instanceof DefaultReactiveConnectionStatus<?> defaultReactiveConnectionStatus)) {
+                return false;
+            }
+            return isManagedByThis(defaultReactiveConnectionStatus);
+        }
+
+        @SuppressWarnings("unchecked")
+        private boolean isManagedByThis(DefaultReactiveConnectionStatus<?> connectionStatus) {
+            return ((DefaultReactiveConnectionStatus<Stage.Session>) connectionStatus).isConnectionOf(this);
+        }
+    };
+
     private final Stage.SessionFactory sessionFactory;
 
     public TxManager(SessionFactory sessionFactory) {
@@ -25,49 +54,59 @@ public class TxManager implements ReactiveTransactionOperations<Stage.Session> {
     }
 
     @Override
-    public <T> Publisher<T> withTransaction(TransactionDefinition definition, TransactionalCallback<Stage.Session, T> handler) {
+    public <T> Publisher<T> withTransaction(TransactionDefinition definition,
+                                            TransactionalCallback<Stage.Session, T> handler) {
         return Mono.fromCompletionStage(() -> sessionFactory.withTransaction((session, transaction) -> {
             try {
                 return Mono.from(handler.doInTransaction(new ReactiveTransactionStatus<>() {
-                        @Override
-                        public Stage.Session getConnection() {
-                            return session;
-                        }
+                    @Override
+                    public Stage.Session getConnection() {
+                        return session;
+                    }
 
-                        @Override
-                        public @NonNull ConnectionStatus<Stage.Session> getConnectionStatus() {
-                            return new DefaultConnectionStatus<>(session, new DefaultConnectionDefinition(ConnectionDefinition.Propagation.REQUIRED), true);
-                        }
+                    @Override
+                    public @NonNull ConnectionStatus<Stage.Session> getConnectionStatus() {
+                        return new DefaultReactiveConnectionStatus<>(
+                            session,
+                            new DefaultConnectionDefinition(ConnectionDefinition.Propagation.REQUIRED),
+                            connectionOperations,
+                            true
+                        );
+                    }
 
-                        @Override
-                        public boolean isNewTransaction() {
-                            return true;
-                        }
+                    @Override
+                    public boolean isNewTransaction() {
+                        return true;
+                    }
 
-                        @Override
-                        public void setRollbackOnly() {
-                            transaction.markForRollback();
-                        }
+                    @Override
+                    public void setRollbackOnly() {
+                        transaction.markForRollback();
+                    }
 
-                        @Override
-                        public boolean isRollbackOnly() {
-                            return transaction.isMarkedForRollback();
-                        }
+                    @Override
+                    public boolean isRollbackOnly() {
+                        return transaction.isMarkedForRollback();
+                    }
 
-                        @Override
-                        public boolean isCompleted() {
-                            return false;
-                        }
+                    @Override
+                    public boolean isCompleted() {
+                        return false;
+                    }
 
-                        @Override
-                        public @NonNull TransactionDefinition getTransactionDefinition() {
-                            return definition;
-                        }
-                    })).contextWrite(context -> context.put(SESSION_KEY, session))
-                    .toFuture();
+                    @Override
+                    public @NonNull TransactionDefinition getTransactionDefinition() {
+                        return definition;
+                    }
+                })).contextWrite(context -> context.put(SESSION_KEY, session)).toFuture();
             } catch (Exception e) {
                 throw new RuntimeException(e);
             }
         }));
+    }
+
+    @Override
+    public boolean managesTransaction(@NonNull ReactiveTransactionStatus<Stage.Session> transactionStatus) {
+        return connectionOperations.managesConnection(transactionStatus.getConnectionStatus());
     }
 }
