96d9bf73126401746f087de42992301a613819e9
Radovan Radic <radicr@gmail.com>
2026-03-11 09:47:00 +0100

    Update to micronaut-data 5.0 (#1862)
    * Update to micronaut-data 5.0
    
    * Workaround for JOOQ transaction propagation
    
    * Fix checkstyle
    
    ---------
    
    Co-authored-by: Denis Stepanov <denis.s.stepanov@oracle.com>

--- jooq/src/main/java/io/micronaut/configuration/jooq/DSLContextFactory.java
+++ jooq/src/main/java/io/micronaut/configuration/jooq/DSLContextFactory.java
@@ -18,8 +18,11 @@ package io.micronaut.configuration.jooq;
 import io.micronaut.context.annotation.EachBean;
 import io.micronaut.context.annotation.Factory;
 import io.micronaut.core.annotation.Internal;
+import io.micronaut.transaction.TransactionStatus;
+import io.micronaut.transaction.support.ExceptionUtil;
 import org.jooq.Configuration;
 import org.jooq.DSLContext;
+import org.jooq.TransactionalCallable;
 import org.jooq.impl.DefaultDSLContext;
 
 /**
@@ -40,7 +43,28 @@ final class DSLContextFactory {
      */
     @EachBean(Configuration.class)
     DSLContext dslContext(Configuration configuration) {
-        return new DefaultDSLContext(configuration);
+        return new DefaultDSLContext(configuration) {
+
+            @Override
+            public <T> T transactionResult(TransactionalCallable<T> transactional) {
+                return super.transactionResult(new TransactionalCallable<T>() {
+                    @Override
+                    public T run(Configuration configuration) throws Throwable {
+                        io.micronaut.transaction.TransactionStatus<?> transactionStatus = (TransactionStatus<?>) configuration.data().get(MicronautTransactionProvider.TX_KEY);
+                        if (transactionStatus != null) {
+                            return transactionStatus.propagate(() -> {
+                                try {
+                                    return transactional.run(configuration);
+                                } catch (Throwable e) {
+                                    return ExceptionUtil.sneakyThrow(e);
+                                }
+                            });
+                        }
+                        return transactional.run(configuration);
+                    }
+                });
+            }
+        };
     }
 
 }
