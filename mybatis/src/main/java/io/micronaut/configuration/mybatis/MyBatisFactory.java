dd89ad016f84eb4ea9f1676c859b9e678bfc2a28
Graeme Rocher <graeme.rocher@oracle.com>
2026-04-17 16:09:08 +0200

    correct

--- mybatis/src/main/java/io/micronaut/configuration/mybatis/MyBatisFactory.java
+++ mybatis/src/main/java/io/micronaut/configuration/mybatis/MyBatisFactory.java
@@ -52,7 +52,7 @@ public class MyBatisFactory {
     @EachBean(DataSource.class)
     public Configuration myBatisConfiguration(
         @Parameter String name,
-        DataSource dataSource,
+        @Parameter DataSource dataSource,
         @Parameter @Nullable TransactionFactory transactionFactory,
         ApplicationContext applicationContext
     ) {
