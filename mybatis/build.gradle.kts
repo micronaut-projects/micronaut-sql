7e5c08f4f8daf15b55ad961f3581bd0ddbf4b6c8
Sergio del Amo <sergio.del.amo.caballero@oracle.com>
2026-07-03 16:28:03 +0200

    use kotlin DSL

--- mybatis/build.gradle.kts
+++ mybatis/build.gradle.kts
@@ -1,22 +1,20 @@
 plugins {
-    id 'io.micronaut.build.internal.sql-module'
+    id("io.micronaut.build.internal.sql-module")
 }
 dependencies {
-    api projects.micronautJdbc
+    api(projects.micronautJdbc)
     api(mn.micronaut.context)
     api(mn.micronaut.inject)
     api(libs.managed.mybatis)
-
     testAnnotationProcessor(mn.micronaut.inject.java)
     testImplementation(mnTest.micronaut.test.junit5)
-
-    testRuntimeOnly projects.micronautJdbcHikari
+    testRuntimeOnly(projects.micronautJdbcHikari)
     testRuntimeOnly(libs.managed.h2)
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+    testRuntimeOnly(mnTest.junit.jupiter.engine)
     testRuntimeOnly(mnTest.junit.platform.launcher)
 }
 
-tasks.named("test", Test) {
+tasks.withType<Test> {
     useJUnitPlatform()
 }
 
