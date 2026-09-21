424d0c3ee9f00e31970a6763617a6ed306af2153
radovanradic <radicr@gmail.com>
2026-09-21 23:26:58 +0200

    Use catalog H2 version in SQL module tests and the OSS Index exclusion.

--- jdbc-tomcat/src/test/groovy/io/micronaut/configuration/jdbc/tomcat/DatasourceConfigurationSpec.groovy
+++ jdbc-tomcat/src/test/groovy/io/micronaut/configuration/jdbc/tomcat/DatasourceConfigurationSpec.groovy
@@ -226,7 +226,7 @@ class DatasourceConfigurationSpec extends Specification {
         String version = resultSet.getString(1)
 
         then:
-        version == '2.5.250'
+        version == System.getProperty('h2.version')
 
         cleanup:
         applicationContext.close()
